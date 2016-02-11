b49ec2b4e26b1f83f17a14a5797683629b672a5d
Wasknijper <mudkippers@hotmail.com>
2016-02-11 10:15:53 +0100

    A working Pokedex!

--- week2-opdrachten/static/app.js
+++ week2-opdrachten/static/app.js
@@ -1,104 +1,221 @@
 (function(){
 	"use strict";
-
 	var app = {
+		apiUrl : 'http://pokeapi.co/',
 		startUrl : "",
 		pages : "",
-		startScreen: "",
+		startScreen: "home",
+		oldRoute : "",
+		newRoute: "",
 		init: function(){
 			this.startUrl = window.location.href;
 
 			//get all the section elements
 			this.pages = document.getElementsByTagName('section');
-			this.startScreen = this.pages.home;
 
 			//Loop through all page elements and make them invisible 
 			for(var i = 0; i < this.pages.length; i++){
 				this.pages[i].classList.add("invis");
 			}
 
+			var pokeData, curObj, name, loading;
+			//check if pokedex is in local storage - bron:http://stackoverflow.com/questions/3262605/html5-localstorage-check-if-item-is-set
+			if(localStorage.getItem('pokedex') === null) {
+				pokeData = this.request('api/v1/pokedex/1');
+				curObj = this;
+
+				pokeData.then(
+				    // success handler
+				    //xhr = xml http request
+				    function(data, xhr) {
+				      // load the list of pokemon into the pokedex, since it contains all the pokemon its the national pokedex.
+				      // its not a string, so no need to parse the JSON
+				      pokedex.national = data.pokemon;
+				      localStorage.pokedex = JSON.stringify(pokedex.national);
+				      //log to check
+				      console.log(pokedex.national);
+				      console.log(localStorage.pokedex);
+				      home.innerHTML = templateHome.render({pokemon : pokedex.national});
+				      pokedex.loaded = true;
+
+				    },
+				    // error handler
+				    function(data, xhr) {
+				      console.error(data, xhr.status);
+				    }
+				);
+			} else {
+				//if the pokedex already exists in localStorage, set it to pokedex.national
+				pokedex.national = JSON.parse(localStorage.pokedex);
+
+				if(localStorage.getItem('savedPokemon')){
+					pokedex.savedPokemon = JSON.parse(localStorage.savedPokemon);
+				}
+
+				console.log(pokedex.national);
+				console.log(pokedex.savedPokemon);
+
+				pokedex.loaded = true;
+				home.innerHTML = templateHome.render({pokemon : pokedex.national});
+				//var pokemon = this.national.indexOf(searchElement[, fromIndex = 0])
+			}
+
 			//add route event listeners by initializing routes
-			routes.init();
+			//routes.init();
+			//initialize pokedex
+			processHash();
+			window.addEventListener('hashchange', processHash);
+		},
+		request : function(options){
+			//call pegasus libirary for the ajax get
+			return pegasus(this.apiUrl + options);
 		}
 	};
 
-	var routes = {
-		newUrl : "",
-		oldUrl : "",
-		init: function(){
-			//run start function from sections to show the correct section to start with
-			sections.start();
-			//https://developer.mozilla.org/en-US/docs/Web/Events/hashchange
-			window.addEventListener('hashchange', function(e){
-				//get the hash from the url, event listener passes the url in the e parameter
-				this.newUrl = e.newURL.split('#')[1];
-				this.oldUrl = e.oldURL.split('#')[1];
-				//call the toggle method from sections to show the correct section
-				sections.toggle(this.newUrl, this.oldUrl);
-			});
+	var routes = new Rlite();
+
+	routes.add('', function () {
+  		document.title = 'Home';
+  		app.pages.home.classList.remove("invis");
+  		console.log(pokedex.national);
+  		if (pokedex.loaded === false){
+  			app.pages.home.innerHTML = templateLoading.render();
+  		} else {
+  			app.pages.home.innerHTML = templateHome.render({pokedex : pokedex.national});
+   		}
+	});
+
+	routes.add('pokemon/:name', function (route) {
+		var pName = route.params.name.toString();
+		console.log(pName);
+		var found = pokedex.findPokemon(pokedex.national, pName);
+		var url, currentPokemon;
+
+		if (found === false){
+
+		} else {
+  			app.pages.pokemon.innerHTML = templateLoadingPoke.render({name : pName});
+			url = pokedex.national[found].resource_uri;
+			pokedex.loadPokemon(pName.capitalize(), url);
 		}
-	};
+
+	});
+
+	// Hash-based routing
+	function processHash() {
+  		var hash = location.hash || '#';
+  		app.oldRoute = app.newRoute;
+  		app.newRoute = hash.slice(1);
+  		routes.run(app.newRoute);
+  		sections.toggle(app.newRoute, app.oldRoute);
+	}
+
+	// var routes = {
+	// 	newUrl : "",
+	// 	oldUrl : "",
+	// 	init: function(){
+	// 		//run start function from sections to show the correct section to start with
+	// 		sections.start();
+	// 		//https://developer.mozilla.org/en-US/docs/Web/Events/hashchange
+	// 		window.addEventListener('hashchange', function(e){
+	// 			//get the hash from the url, event listener passes the url in the e parameter
+	// 			this.newUrl = e.newURL.split('#')[1];
+	// 			this.oldUrl = e.oldURL.split('#')[1];
+	// 			//call the toggle method from sections to show the correct section
+	// 			sections.toggle(this.newUrl, this.oldUrl);
+	// 		});
+	// 	}
+	// };
 
 	var sections = {
-		start: function(){
-			//check if the url contains a hash, returns -1 if false, anything else is true
-			if (app.startUrl.indexOf('#') >= 0){
-				var hashName = app.startUrl.split('#')[1];
-				//remove the invis class from the section
-				app.pages[hashName].classList.remove('invis');	
-			} else {
-				//if no hash found its on the startscreen, so remove the invis class from that
-				app.startScreen.classList.remove("invis");					
-			}
-		},
 		toggle: function(newRoute, oldRoute){
 			//scroll to top incase the page is long
 			window.scrollTo(0,0);
-
+			newRoute = newRoute.split('/')[0];
+			oldRoute = oldRoute.split('/')[0];
+			//console.log(newRoute);
 			//check if the newRoute is the same as the old route, only excecute if its changes
 			if (newRoute != oldRoute) {
-				app.pages[newRoute].classList.remove('invis');
-
+				if (newRoute){
+					app.pages[newRoute].classList.remove('invis');
+				} else {
+					//if there is no hash then set it to the home
+					app.pages[app.startScreen].classList.remove('invis');
+				}
 				//add invis to the old route, if it exists
 				if(oldRoute){
 					app.pages[oldRoute].classList.add('invis');	
 				} else {
 					//if it doesnt, its on the startscreen, so add invis to that
-					app.startScreen.classList.add('invis');
+					app.pages[app.startScreen].classList.add('invis');
 				}
 			}
+
 		}
 	};
 
-	var data = {
-		baseUrl : 'http://pokeapi.co/api/v1/',
-		request : function(base, options){
-			return pegasus(base + options);
-		}
-	};
+	var templateLoading = new t("<p>Loading all the Pokémon...</p>");
+	var templateLoadingPoke = new t("<p>Loading data on {{=name}}</p>");
+	var templateHome = new t("<ul>{{@pokedex}}<li><a href=\'#pokemon/{{%_val.name}}\'>{{=_val.name}}</a></li>{{/@pokedex}}</ul>");
+	var templateError = new t("<p>This is not a pokemon! <a href=\'#\'>Return to Home</a></p>");
+	var templatePokemonEvolve = new t("<h1>Meet {{=pokemon.name}}!</h1> <section> <h2>Abilities</h2> {{@pokemon.abilities}}<li>{{=_val.name}}</a></li>{{/@pokemon.abilities}}</section> <section> <li>Attack: {{=pokemon.attack}}</li><li>Defence: {{=pokemon.defence}}</li><li>Hitpoints: {{=pokemon.hp}}</li><li>Sp. Attack: {{=pokemon.sp_atk}}</li><li>Sp. Defence: {{=pokemon.sp_def}}</li><li>Speed: {{=pokemon.speed}}</li></section> <section>{{=pokemon.name}} evolves to <a href=\'#pokemon/{{=evoLink}}\'>{{=evolution}}</a></section>");
+	var templatePokemon = new t("<h1>Meet {{=pokemon.name}}!</h1> <section> <h2>Abilities</h2> {{@pokemon.abilities}}<li>{{=_val.name}}</a></li>{{/@pokemon.abilities}}</section> <section> <li>Attack: {{=pokemon.attack}}</li><li>Defence: {{=pokemon.defence}}</li><li>Hitpoints: {{=pokemon.hp}}</li><li>Sp. Attack: {{=pokemon.sp_atk}}</li><li>Sp. Defence: {{=pokemon.sp_def}}</li><li>Speed: {{=pokemon.speed}}</li></section>");
 
 	var pokedex = {
+		//pokemon objects in the national array have a name(bulbasaur) and a resource_uri("api/v1/pokemon/1/")
+		loaded : false,
 		national : [],
-		loadedPokemon : [],
+		savedPokemon : [],
+		loadPokemon : function(pName, uri, pokemon) {
+			var result = app.request(uri);
+			var curObj = this;
+			var isSaved = this.findPokemon(this.savedPokemon, pName);
+
+			if (isSaved === false){
+				result.then(
+					function(data, xhr) {
+						var pokemon = data;
+						console.log(data);
+						curObj.savedPokemon.push(data);
+						localStorage.savedPokemon = JSON.stringify(curObj.savedPokemon);
+						//return the loaded pokemon so we can use it in a function
+						curObj.displayPokemon(pokemon);
+					}, function(data, xhr) {
+						console.error(data, xhr.status);
+					}
+				);
+			} else {
+				console.log(this.savedPokemon[isSaved]);
+				this.displayPokemon(this.savedPokemon[isSaved]);
+			}
+
+		},
+		findPokemon : function(array, value){
+			if(array.length !== 0){
+				for(var i = 0, len = array.length; i < len; i++) {
+    				if( array[i].name == value ){
+        				return i;
+					} else if (array.length-1 === i){
+						return false;
+					}
+				}
+			} else {
+				return false;
+			}
+		},
+		displayPokemon : function(pokemon){
+			if (pokemon.evolutions.length > 0){
+	  			app.pages.pokemon.innerHTML = templatePokemonEvolve.render({pokemon : pokemon, evolution: pokemon.evolutions[0].to, evoLink : pokemon.evolutions[0].to.toLowerCase()});
+	  		} else {
+	  			app.pages.pokemon.innerHTML = templatePokemon.render({pokemon : pokemon});	
+	  		}
+		}
 	};
 
-	var pokedexData = data.request(data.baseUrl, 'pokedex/1');
-	
-	pokedexData.then(
-	    // success handler
-	    function(data, xhr) {
-	      // load the list of pokemon into the pokedex, since it contains all the pokemon its the national pokedex.
-	      // its not a string, so no need to parse the JSON
-	      pokedex.national = data;
-
-	      //log to check
-	      console.log(pokedex.national);
-	    },
-	    // error handler (optional)
-	    function(data, xhr) {
-	      console.error(data, xhr.status);
-	    }
-	  );
+	//add capitalize function to string, source: http://stackoverflow.com/a/3291856
+	String.prototype.capitalize = function() {
+    	return this.charAt(0).toUpperCase() + this.slice(1);
+	};
 
 	//start the app
 	app.init();
