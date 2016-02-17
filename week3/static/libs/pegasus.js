ce52c5be95c0af494df66b68ef671d9ad3991f00
Wasknijper <mudkippers@hotmail.com>
2016-02-17 13:16:21 +0100

    Nevermind, looking into other solution to Voice Recognition

--- week3/static/libs/pegasus.js
+++ week3/static/libs/pegasus.js
@@ -5,10 +5,6 @@ function pegasus(a, xhr) {
 
   // Open url
   xhr.open('GET', a);
-  xhr.setRequestHeader("Access-Control-Allow-Origin", "https://wasknijper.github.io/MWD-WebAppFromScratch/week3/");
-  xhr.setRequestHeader("Access-Control-Allow-Methods", "GET");
-  xhr.setRequestHeader("Access-Control-Max-Ag", "1728000");
-  xhr.setRequestHeader("Access-Control-Allow-Headers", "Content-Type, Connection, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
   
   // Reuse a to store callbacks
   a = [];
